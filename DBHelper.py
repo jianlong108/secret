cef5c0cf5a38872b4d68fa1dbc3c8d166b4ab82f
wangjianlong <wangjianlong@xiaomi.com>
2017-05-19 18:51:09 +0800

    将联赛数据保存数据库

--- DBHelper.py
+++ DBHelper.py
@@ -206,6 +206,9 @@ def getGameData(game):
     loseCount = 0
     num = 0
 
+    handi_win_count = 0
+    handi_draw_count = 0
+    handi_lose_count = 0
 
 
     if game.handiCompanies == None:
@@ -220,27 +223,29 @@ def getGameData(game):
         r = c.fetchall()
         num += len(r)
         for result in r:
+            if result[3] == 3:
+                winCount += 1
+            elif result[3] == 1:
+                drawCount += 1
+            else:
+                loseCount += 1
+
             if oneCompany.now >= 0:
                 if result[4] - result[5] - float(oneCompany.now) > 0:
-                    winCount += 1
+                    handi_win_count += 1
                 elif int(result[4]) - int(result[5]) - float(oneCompany.now) == 0:
-                    drawCount += 1
+                    handi_draw_count += 1
                 else:
-                    loseCount += 1
+                    handi_lose_count += 1
             else:
                 if int(result[5]) - int(result[4]) + float(oneCompany.now) > 0:
-                    winCount += 1
+                    handi_win_count += 1
                 elif int(result[5]) - int(result[4]) + float(oneCompany.now) == 0:
-                    drawCount += 1
+                    handi_draw_count += 1
                 else:
-                    loseCount += 1
+                    handi_lose_count += 1
 
-           # if result[3] == 3:
-           #     winCount += 1
-           # elif result[3] == 1:
-           #     drawCount += 1
-           # else:
-           #     loseCount += 1
+
 
 
 
@@ -248,8 +253,16 @@ def getGameData(game):
         if float(winCount) / float(num) > 0.4 or float(drawCount) / float(num) > 0.4 or float(loseCount) / float(num) > 0.4:
             tempstr = ''.join(['  亚 ', '赢盘: ', str(float(winCount) / float(num) * 100)[:5], '/100', ' 走盘:', str(float(drawCount) / float(num) * 100)[:5], '/100', '输盘', str(float(loseCount) / float(num) * 100)[:5], '/100  '])
             contentstr = contentstr + tempstr
+            tempstr = ''.join(['  亚: ', '胜: ', str(float(winCount) / float(num) * 100)[:5], '/100', ' 平:',
+                               str(float(drawCount) / float(num) * 100)[:5], '/100', '负',
+                               str(float(loseCount) / float(num) * 100)[:5], '/100'])
+            contentstr = contentstr + tempstr
             print str(game.beginTime) + ':' + game.leauge +':'+ game.homeTeam + 'vs' + game.friendTeam
-            print ' 赢盘:' + str(float(winCount)/float(num) * 100)[:5]+'/100' + ' 走盘:' + str(float(drawCount)/float(num) * 100)[:5]+'/100' + ' 输盘:' + str(float(loseCount)/float(num) * 100)[:5]+'/100'
+            print ' 赢盘:' + str(float(handi_win_count)/float(num) * 100)[:5]+'/100' + ' 走盘:' + str(float(handi_draw_count)/float(num) * 100)[:5]+'/100' + ' 输盘:' + str(float(handi_lose_count)/float(num) * 100)[:5]+'/100'
+            print ' 亚 胜:' + str(float(winCount) / float(num) * 100)[
+                      :5] + '/100' + ' 平:' + str(
+            float(drawCount) / float(num) * 100)[:5] + '/100' + ' 负:' + str(float(loseCount) / float(num) * 100)[
+                                                                        :5] + '/100'
     # 欧赔
     winCount = 0
     drawCount = 0
